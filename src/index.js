16df511f36d56e0f4324804018e72ac9702f3b2a
Robert Clarke <r.jason.clarke@gmail.com>
2021-04-15 20:10:41 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -35,5 +35,8 @@ function render() {
   }
 };
 
+let store = createStore(candyReducer)
+store.dispatch({type: "INIT"})
+
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
